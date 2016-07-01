d1b99f8975c61e7881f8ae98c3bd3721cbca2d51
yorhodes <yorkerhodesiv@gmail.com>
2016-07-01 09:57:03 -0400

    wrote functions for editing table

--- public/javascripts/table_manager.js
+++ public/javascripts/table_manager.js
@@ -1,1 +1,14 @@
-$("#table_body")
+function create_entry(_block_number, _borrower, _amount, _collateral, _fee) {
+	var row = "<tr id=" + _block_number;
+	row.concat("<td>" + _block_number + "</td>");
+	row.concat("<td>" + _borrower + "</td>");
+	row.concat("<td>" + _amount + "</td>");
+	row.concat("<td>" + _collateral + "</td>");
+	row.concat("<td>" + _fee + "</td>");
+	row.concat("</tr>")
+	$("#table_body").append(row);
+}
+
+function delete_entry(_block_number) {
+	$("#" + _block_number).remove();
+}
